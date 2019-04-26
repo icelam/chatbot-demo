481139707467c4817b7e490f58fe246fc25d8e4f
Ice Lam <pinky.yy.lam@gmail.com>
2019-04-27 00:41:39 +0800

    Update chatbot icon license message

--- chatbot-ui/src/components/Chatbot/ChatbotInfo.js
+++ chatbot-ui/src/components/Chatbot/ChatbotInfo.js
@@ -18,10 +18,10 @@ const ChatbotInfo = ({ showInfo, actions }) => (
       'This is a simple chatbot demo built with Node.js (Express), node-nlp, ReactJS and Redux.',
       '',
       [
-        'The chatbot icons made by ',
+        'The chatbot profile picture is made by ',
         <a href="https://www.freepik.com/" title="Freepik" target="_blank" rel="noopener noreferrer" key="freepik">Freepik</a>,
         ' from ', <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer" key="flaticon">www.flaticon.com</a>,
-        ' is licensed by ', <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer" key="cc">CC 3.0 BY</a>
+        ', licensed by ', <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer" key="cc">CC 3.0 BY</a>
       ]
     ]}
     show={showInfo}
