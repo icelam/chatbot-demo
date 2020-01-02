3d2747333130f54a7b87f49992e823df818afffa
Ice Lam <pinky.yy.lam@gmail.com>
2020-01-02 17:05:01 +0800

    Update project dependencies to solve possible vulnerabilities

--- chatbot-ui/src/index.js
+++ chatbot-ui/src/index.js
@@ -1,6 +1,6 @@
-import 'babel-polyfill';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+import "core-js/stable";
+import 'core-js/es/map';
+import 'core-js/es/set';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
